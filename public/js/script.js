d3a76b1aabf4806552c741f20ac2c1a12cc0b615
eye <eyemonkey2013@gmail.com>
2024-10-08 23:33:42 +0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+function submitLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Application-Key': 'TU7fac38279f0ffe776b517a6e773ed9991f26ce3081f4903a2c2bb4d0f3efa63d62347501c97cfbd61ac5ddb0c4d29336'
+        },
+        body: JSON.stringify({
+            "UserName": username,
+            "PassWord": password
+        })
+    })
+.then(response => response.json())
+    .then(data => {
+        document.getElementById('message').innerText = data.message;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('message').innerText = 'An error occurred while processing your request.';
+    });
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const roleSelect = document.getElementById('role');
+    const loginButton = document.querySelector('button[type="button"]:nth-of-type(1)');
+
+    function checkInputs() {
+        const isUsernameFilled = usernameInput.value.trim() !== '';
+        const isPasswordFilled = passwordInput.value.trim() !== '';
+        const isRoleSelected = roleSelect.value !== '';
+
+        loginButton.disabled = !(isUsernameFilled && isPasswordFilled && isRoleSelected);
+    }
+
+    usernameInput.addEventListener('input', checkInputs);
+    passwordInput.addEventListener('input', checkInputs);
+    roleSelect.addEventListener('change', checkInputs);
+
+    checkInputs();
+});
